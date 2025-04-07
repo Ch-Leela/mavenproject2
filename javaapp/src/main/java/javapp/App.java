a8b75cb68c881f2140ae99ace5466f34e9a0c41b
root <root@ip-172-31-44-191.eu-north-1.compute.internal>
2025-04-07 05:04:44 +0000

    Initial commit for javaapp project

---
+++ javaapp/src/main/java/javapp/App.java
@@ -0,0 +1,10 @@
+package javapp;
+
+/**
+ * Hello, welcome to Maven
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, welcome to Maven!");
+    }
+}
