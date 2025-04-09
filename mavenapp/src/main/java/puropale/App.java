769021f7743a4d5cd368e5d8bab3135ce9b1862d
root <root@ip-172-31-44-191.eu-north-1.compute.internal>
2025-04-09 09:14:04 +0000

    Intial commits

---
+++ mavenapp/src/main/java/puropale/App.java
@@ -0,0 +1,10 @@
+package puropale;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("\u001B[35mWelcome TO PUROPALE CREATIONS & IT SOLUTIONS\u001B[0m");
+    }
+}
